837c6fd96d32ebacfc6bc475888ef1873124d0ec
Ali Sa <alex.technosun@gmail.com>
2022-04-02 14:22:34 -0700

    adding more files

---
+++ Untitled-2.py
@@ -0,0 +1,4 @@
+name = input ("what is your name ?")
+neighborbname= input ("what is your neighbor’s name?")
+age = int(input("how many months you and your neighbor have been coding?"))
+print(f"my name is {name} and myneghborname is {neighborbname}.we are almost {age}"codding to gether.)
---
+++ world1.py
@@ -0,0 +1,12 @@
+from operator import truediv
+
+
+name= "ali"
+country = "usa"
+age = "42"
+hourlywage = "100"
+satisfied = "true"
+dailywages = hourlywage*8
+print(f"it is {satisfied} that i am satisfied with adaily of {dailywages}")
+
+
